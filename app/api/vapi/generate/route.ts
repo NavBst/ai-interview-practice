4cdc479000f22ca571b0654b11c32678810d18f0
Navneet Bisht <nick49805@gmail.com>
2025-12-09 17:50:33 +0530

    implement interview generation

--- app/api/vapi/generate/route.ts
+++ app/api/vapi/generate/route.ts
@@ -8,11 +8,11 @@ export async function GET() {
 }
 
 export async function POST(request: Request) {
-    console.log(request)
-  const { type, role, level, techstack, amount, userid } =  request.json();
+  console.log(request);
+  const { type, role, level, techstack, amount, userid } = await  request.json();
   try {
     const { text: questions } = await generateText({
-      model: google("gemini-2.0-flash-001"),
+      model: google("gemini-2.5-flash"),
       prompt: `Prepare questions for a job interview.
         The job role is ${role}.
         The job experience level is ${level}.
@@ -23,32 +23,32 @@ export async function POST(request: Request) {
         The questions are going to be read by a voice assistant so do not use "/" or "*" or any other special characters which might break the voice assistant.
         Return the questions formatted like this:
         ["Question 1", "Question 2", "Question 3"]
-        
         Thank you! <3
     `,
     });
 
     const interview = {
-        role, 
-        type, 
-        level, 
-        techstack:techstack.split(","), 
-        amount, 
-        questions: JSON.parse(questions), 
-        userId: userid, 
-        finilized: true, 
-        coverImage: getRandomInterviewCover(), 
-        createdAt: new Date().toISOString()
-    }
+      role,
+      type,
+      level,
+      techstack: techstack.split(","),
+      amount,
+      questions: JSON.parse(questions),
+      userId: userid,
+      finilized: true,
+      coverImage: getRandomInterviewCover(),
+      createdAt: new Date().toISOString(),
+    };
 
     await db.collection("interviews").add(interview);
 
-    return Response.json({success: true}, { status: 200 })
-
-
+    return Response.json({ success: true }, { status: 200 });
   } catch (error) {
     console.error(error);
 
-    return Response.json({ success: false, data: "Something went wrong" }, { status: 500 });
+    return Response.json(
+      { success: false, data: "Something went wrong" },
+      { status: 500 }
+    );
   }
 }
